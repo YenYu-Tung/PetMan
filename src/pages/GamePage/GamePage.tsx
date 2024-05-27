49051ba97c161d03829dcbd4382817b6039645e9
YenYu-Tung <74877687+YenYu-Tung@users.noreply.github.com>
2024-05-27 14:23:04 +0800

    fix: server api

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -24,7 +24,7 @@ export const GamePage: React.FC = observer(() => {
 
   useEffect(() => {
     // ! the game server is running on another port.
-    const socket = io('http://localhost:3001');
+    const socket = io('https://pet-man-server.vercel.app');
 
     // the client sends his or her name to the server
     socket.emit('players', { 'name': (Math.random() + 1).toString(36).substring(7) });
